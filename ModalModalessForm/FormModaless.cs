ca73d5bdec30754bb23f23b726b502abe7b80ef8
YoungSun Shin <tlsdudtjs128@gmail.com>
2019-06-24 13:51:40 +0900

    #1 Modal & Modaless 창 띄우기 by 3110 신영선

---
+++ ModalModalessForm/FormModaless.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ModalModalessForm
+{
+    public partial class FormModaless : Form
+    {
+        public FormModaless()
+        {
+            InitializeComponent();
+        }
+
+        private void FormModaless_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
